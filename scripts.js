d80d0099af891a4f0cdbb7644c10046367f8dcba
Kou-kun42 <shinigamithekido@gmail.com>
2020-10-04 19:28:05 -0400

    finished main part of js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,74 @@
 const spoolcostInput = document.querySelector("#spoolcost-input");
 const spoolweightInput = document.querySelector("#spoolweight-input");
 const filamentlengthInput = document.querySelector("#filamentlength-input");
-const filamentweightInput = document.querySelector("#filamentweight-input");
+const printweightInput = document.querySelector("#printweight-input");
 const diameterSelect = document.querySelector("#diameter-select");
 const materialSelect = document.querySelector("#material-select");
 const printcostInput = document.querySelector("#printcost-input");
 
 spoolcostInput.addEventListener("input", handleCost);
+spoolweightInput.addEventListener("input", handleCost);
+printweightInput.addEventListener("input", handleCost);
+
+filamentlengthInput.addEventListener("input", getWeight);
+
+printweightInput.addEventListener("input", getLength);
+diameterSelect.addEventListener("input", getLength);
+materialSelect.addEventListener("input", getLength);
+
+function handleCost() {
+  let spoolCost = parseFloat(spoolcostInput.value);
+  let spoolWeight = parseInt(spoolweightInput.value);
+  let printWeight = parseFloat(printweightInput.value);
+
+  if (spoolCost < 0 || isNaN(spoolCost)) {
+    spoolcostInput.value = 0;
+    spoolCost = 0;
+  }
+  if (spoolWeight < 0 || isNaN(spoolWeight)) {
+    spoolweightInput.value = 0;
+    spoolWeight = 0;
+  }
+  if (printWeight < 0 || isNaN(printWeight)) {
+    printweightInput.value = 0;
+    printWeight = 0;
+  }
+
+  const printCost = (spoolCost / spoolWeight) * printWeight;
+  if (isNaN(printCost) || printCost === Infinity) {
+    printcostInput.value = 0;
+  } else {
+    printcostInput.value = printCost.toFixed(2);
+  }
+}
+
+function getWeight() {
+  let filamentLength = parseFloat(filamentlengthInput.value);
+  const diameter = parseFloat(diameterSelect.value);
+  const material = parseFloat(materialSelect.value);
+
+  if (filamentLength < 0 || isNaN(filamentLength)) {
+    filamentlengthInput.value = 0;
+    filamentLength = 0;
+  }
+
+  const volume = Math.PI * (diameter / 2) ** 2 * filamentLength;
+  const weight = volume * material;
+  printweightInput.value = parseInt(weight);
+  handleCost();
+}
+
+function getLength() {
+  let weight = parseInt(printweightInput.value);
+  const diameter = parseFloat(diameterSelect.value);
+  const material = parseFloat(materialSelect.value);
+
+  if (weight < 0 || isNaN(weight)) {
+    printweightInput.value = 0;
+    weight = 0;
+  }
+
+  const volume = weight / material;
+  const length = volume / (Math.PI * (diameter / 2) ** 2);
+  filamentlengthInput.value = length.toFixed(2);
+}
